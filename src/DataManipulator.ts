9e8329156c49802385b3c3a74dc57f9ce6008156
David Schurer <david.schurer@yahoo.com>
2024-06-13 19:57:02 -0700

    finished task

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
+// File: src/DataManipulator.ts
+
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row[] {
+    const stockA = serverResponds.find(el => el.stock === 'ABC');
+    const stockB = serverResponds.find(el => el.stock === 'DEF');
+
+    const priceA = stockA && stockA.top_ask ? stockA.top_ask.price : 0;
+    const priceB = stockB && stockB.top_ask ? stockB.top_ask.price : 0;
+
+    const ratio = priceA / priceB;
+
+    const upper_bound = 1 + 0.05;
+    const lower_bound = 1 - 0.05;
+
+    const trigger_alert = (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined;
+
+    return [{
+      ratio,
+      timestamp: new Date(serverResponds[0].timestamp),
+      upper_bound,
+      lower_bound,
+      trigger_alert,
+    }];
   }
 }
